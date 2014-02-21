4680f073ff1c1f09d785a494f2e5d2b8ff43b472
rtrjl <rt@zone42.fr>
2014-02-21 17:14:09 +0100

    fin du chapitre 1

--- chapter-1/cards.c
+++ chapter-1/cards.c
@@ -11,27 +11,38 @@ int main()
 {
 
   char card_name[3];
-  puts("Enter the card_name :");
-  scanf("%2s", card_name );
-  int val = 0;
-  if (card_name[0] == 'K' )
-    {
+  int count = 0;
+  while(card_name[0]!='X'){
+    puts("Enter the card_name :");
+    scanf("%2s", card_name );
+    int val = 0;
+   
+    switch(card_name[0]){
+    case 'K':
+    case 'Q':
+    case 'J':
       val = 10;
-    }
-  else if (card_name[0] == 'Q')
-    {
-      val = 10;
-    }
-  else if (card_name[0] == 'A')
-    {
+      break;
+    case 'A':
       val = 11;
+      break;
+    case 'X':
+      continue;
+    default: 
+        val = atoi(card_name);
+        if ((val<1)||(val>10)){
+          puts("I don't understand that value!");
+          continue;
+        }
     }
-  else 
-    {
-      val = atoi(card_name);
-    }
-  printf("The card value is: %i\n",val);
+    /*Check if the value is 3 to 6*/
+    if ((val > 2)&&(val<7))
+      count++;
+    /* Otherwise check if the card was 10, J, Q, or K */
+    else if (val == 10)
+      count--;
+    printf("Current count: %i\n",count);
+  }
   return 0;
-
 
 }
