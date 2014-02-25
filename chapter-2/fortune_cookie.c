e71a829647c47b0036ff2609f376b158302a23f3
rtrjl <rt@zone42.fr>
2014-02-25 17:30:28 +0100

    hello

--- chapter-2/fortune_cookie.c
+++ chapter-2/fortune_cookie.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_ELEM(x) (sizeof (x) / sizeof (*(x)))
+
 void fortune_cookie(char msg[])
 {
   printf("Message reads: %s\n",msg);
-  printf("msg occupies %lu bytes\n", sizeof(msg));
+  printf("msg occupies %lu bytes\n", NUM_ELEM(msg));
   printf("The quote string is stored at: %p\n",msg);
 
 }
@@ -14,3 +16,4 @@ int main()
   fortune_cookie("Cookies make you fat ");
   return 1;
 }
+
